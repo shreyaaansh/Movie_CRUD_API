d8f881a63040a793b20f90cebb69edcf847ca217
shreyaaansh <85485959+shreyaaansh@users.noreply.github.com>
2023-12-11 21:23:58 +0530

    Add files via upload
    Added few movies to check the API working and the functions

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
-import(
-	"fmt"
-	"log"
+import (
 	"encoding/json"
-	"math/rand"
+	"log"
 	"net/http"
-	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
@@ -25,12 +23,24 @@ type Director struct{
 
 var movies []Movie
 
+
+func getMovies(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
 func main(){
 	r := mux.NewRouter()
 	
+
+	movies = append(movies, Movie{ID: "1", Isbn:"438227", Title:"Movie One", Director: &Director{Firstname: "Karan", Lastname: "Johar"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "45455", Title:"Movie Two", Director: &Director{Firstname: "Christopher", Lastname: "Nolan"}})
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000",r))
 }
